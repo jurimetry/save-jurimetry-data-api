0772aa3385ff210e98acb22d469cb7c036fd0813
Renan Winter Spatin <rwspatin@gmail.com>
2020-12-27 00:25:57 -0300

    Do not add jurisprudences that alread exists

--- src/app/controllers/JurisprucenceController.js
+++ src/app/controllers/JurisprucenceController.js
@@ -2,6 +2,21 @@ import Jurisprudence from '../models/Jurisprudence';
 
 class JurisprudenceController {
     async store(req, res) {
+        const jurisprudenceExists = await Jurisprudence.findOne({
+            where: {
+                court_abreviation: req.body.court_abreviation,
+                decision_date: req.body.decision_date,
+                class: req.body.class,
+                decision: req.body.decision,
+                court_decisor: req.body.court_decisor,
+                court_session: req.body.court_session,
+            },
+        });
+
+        if (jurisprudenceExists) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
         const jurisprudence = await Jurisprudence.create(req.body);
 
         return res.json(jurisprudence);
