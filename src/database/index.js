ddb27d1d8e04592cb5aa666f038b86ea3eb7c5ae
Renan Winter Spatin <rwspatin@gmail.com>
2020-12-27 00:04:58 -0300

    Create database models connection

---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+
+import Jurisprudence from '../app/models/Jurisprudence';
+
+import databaseConfig from '../config/database';
+
+const models = [Jurisprudence];
+
+class Database {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.connection = new Sequelize(databaseConfig);
+
+        models.map((model) => model.init(this.connection));
+    }
+}
+
+export default new Database();
