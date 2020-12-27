749778ed9556839964db6943b0be180d77b13951
Renan Winter Spatin <rwspatin@gmail.com>
2020-12-26 23:56:27 -0300

    correct host db

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
 module.exports = {
     dialect: 'postgres',
     host: 'ec2-52-44-139-108.compute-1.amazonaws.com',
-    username: 'gskutxpzrujbls',
-    password:
-        '74ae60662e5109135465d0bd124188a54cfbebe2efd4077885692c6efd1b324d',
+    username: 'username',
+    password: 'password',
     database: 'ddhhqf3lrh36s1',
     define: {
         timestamps: true,
